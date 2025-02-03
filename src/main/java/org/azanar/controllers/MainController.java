299c6189122da528ad950adefa59ad6182cfa5e4
Martin Šipkovský <63198333+martinsipkovsky@users.noreply.github.com>
2025-02-03 17:08:30 +0100

    webserver

---
+++ src/main/java/org/azanar/controllers/MainController.java
@@ -0,0 +1,13 @@
+package org.azanar.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public @ResponseBody String index() {
+        return "index.html";
+    }
+}
