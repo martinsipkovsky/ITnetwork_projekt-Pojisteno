b5e9ec68727c779bcd92e7339d703b86f5ae9cf1
Martin Šipkovský <63198333+martinsipkovsky@users.noreply.github.com>
2025-02-04 20:44:06 +0100

    webUI

--- src/main/java/org/azanar/Main.java
+++ src/main/java/org/azanar/Main.java
@@ -3,9 +3,14 @@ package org.azanar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/pojisteno?user=root&password=");
         SpringApplication.run(Main.class, args);
     }
 }
